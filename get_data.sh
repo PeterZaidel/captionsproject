69e8a365a34ea850fb4470c370b27fbc9317e66a
forpostt <p.mankevich@sphere-ai0.g>
2018-05-07 11:22:10 +0300

    del trash

--- get_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bash/bin
-mkdir coco_dataset
-cd ./coco_dataset
-wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-unzip train2017.zip
-unzip val2017.zip
-unzip annotations_trainval2017.zip
--- preinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bash/bin
-git clone https://github.com/pdollar/coco.git
-cd coco/PythonAPI
-make
-sudo make install
-sudo python setup.py install
